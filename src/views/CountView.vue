b8b087c44335a8ffb2dc77125b8b1e417cfd8386
Hieu Huynh <hieu.huynh08092000@gmail.com>
2022-02-14 18:54:28 +0700

    first commit

---
+++ src/views/CountView.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { defineComponent, nextTick } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      count: 1,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+
+  mounted() {
+    this.count = 2;
+  },
+
+  computed: {
+    log() {
+        console.log('log',this.count);
+    } 
+  },
+});
+</script>
+
+<template>
+  <button class="buttonCount" @click="increment">{{ count }}</button>
+</template>
+
+<style scoped>
+.buttonCount {
+  height: 32px;
+  width: 70px;
+}
+</style>
